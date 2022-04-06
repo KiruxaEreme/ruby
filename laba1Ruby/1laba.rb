9125fca5c031ac13bf3050c0b3cba6e60c38e2b4
KiruxaEreme <78785627+KiruxaEreme@users.noreply.github.com>
2022-04-06 14:08:56 +0300

    1block(3-4)

--- laba1Ruby/1laba.rb
+++ laba1Ruby/1laba.rb
@@ -1,1 +1,35 @@
+def select_language(language)
+  case language
+  when "Ruby"
+    puts "Ну ты и подлиза!"
+  when "C"
+    puts "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+  when "C++"
+    puts "Последние нововведения в C++ были созданы, чтобы исправить предыдущие нововведения."
+
+  else
+    puts "Не понял, но скоро будет Ruby."
+  end
+end
+
+
 puts "Hello world"
+a=ARGV[0]#АРГУМЕНТ ПРОГРАММЫ
+puts "Hello #{a}!"
+ARGV.clear
+k=gets.chomp
+puts k
+puts "Какой ваш любимый язык?"
+language = gets.chomp # Считываем язык
+select_language(language) # Вызов выбора ответа
+
+puts "Введите команду ОС"
+command_os = system"#{gets}"
+puts command_os
+
+puts "Введите команду Ruby"
+command_ruby = `ruby "-e#{gets}"`
+puts command_ruby
+
+
+# Ответ на введенный язык
